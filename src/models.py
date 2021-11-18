19a9d13a39be488eddb068300244ec2678684663
elianajr <elianajr1@gmail.com>
2021-11-18 16:24:50 +0000

    user

--- src/models.py
+++ src/models.py
@@ -2,7 +2,30 @@ import simplejson as json
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
- 
+
+class User(db.Model):
+    __tablename__: "user"
+
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), unique=False, nullable=False)
+    _password = db.Column(db.String(120), unique=False, nullable=False)
+    _is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+
+    def __repr__(self):
+        return f'User {self.email}, id: {self.id}' 
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "email": self.email
+        }
+
+    @classmethod
+    def get_by_email(cls,email):
+        user = cls.query.filter_by(email=email).one_or_none()
+        return user
+
+
 class StarshipsDetails(db.Model):
     __tablename__: "starships_details"
 
